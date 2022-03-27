f331a92033fa67cd2e561d8aceaa33341b5a29f4
rpania30 <85898157+rpania30@users.noreply.github.com>
2022-03-27 03:52:03 -0400

    HTTP COmmit

--- server/server.js
+++ server/server.js
@@ -1,5 +1,5 @@
-const HTTPS_PORT = 8443; //default port for https is 443
-const HTTP_PORT = 8001; //default port for http is 80
+const HTTPS_PORT = 8443; //default port for https is 8443
+const HTTP_PORT = 80; //default port for http is 80
 
 const fs = require('fs');
 const http = require('http');
